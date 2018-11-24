f7ce3dee3a459f780b0f0c665375aa723425da2a
dibyadarshan hota <dibyadarshan.hota@gmail.com>
2018-11-24 15:10:21 +0530

    Screenshots added

--- serial.cc
+++ serial.cc
@@ -1,47 +1,47 @@
 #include <bits/stdc++.h>
 
-#define n_len 10000000
-#define e_len 10000000
+#define n_len 100005
+#define e_len 100005
 
 using namespace std;
 
 // reset queue
-void clear( std::queue<int> &q ) {
-   std::queue<int> empty;
+void clear( std::queue<long long int> &q ) {
+   std::queue<long long int> empty;
    std::swap( q, empty );
 }
 // reset stack
-void clear_stack( std::stack<int> &s ) {
-   std::stack<int> empty;
+void clear_stack( std::stack<long long int> &s ) {
+   std::stack<long long int> empty;
    std::swap( s, empty );
 }
 
 // variables
-int V[n_len], E[e_len], sigma[n_len], dist[n_len];
+long long int V[n_len], E[e_len], sigma[n_len], dist[n_len];
 float cb[n_len], delta[n_len];
-stack<int> s;
-queue<int> q;
+// stack<long long int> s;
+// queue<long long int> q;
 
 // MAIN
 int main() {
 
     // ================================ READ INPUT AND MAKE Compressed Adjancency List ====================================
-    int nodes, edges;
+    long long int nodes, edges;
     cin>>nodes>>edges;
-    vector<vector<int> > p(nodes);
+    vector<vector<long long int> > p(nodes);
         
-	// vector<int> V(nodes+1);
-	// vector<int> E(2*edges);
+	// vector<long long int> V(nodes+1);
+	// vector<long long int> E(2*edges);
     // read graph data in CSR format 
     string line;
-    int node = 0;
-    int counter = 0;
+    long long int node = 0;
+    long long int counter = 0;
     getline(cin, line);
-    for (int i = 0; i < nodes; ++i) {
+    for (long long int i = 0; i < nodes; ++i) {
         getline(cin, line);
         V[node] = counter;
         istringstream is(line);
-        int tmp;
+        long long int tmp;
         while (is >> tmp) {
             E[counter] = tmp;
             counter += 1;
@@ -50,11 +50,11 @@ int main() {
     }
     V[node] = counter;
     // Check compressed adj list
-    // for (int i = 0; i <= nodes; i++) {
+    // for (long long int i = 0; i <= nodes; i++) {
     //     cout<<V[i]<<" ";
     // }
     // cout<<"\n";
-    // for (int i = 0; i < 2 * edges ; ++i) {
+    // for (long long int i = 0; i < 2 * edges ; ++i) {
     //     cout<<E[i]<<" ";
     // }
     // cout<<"\n";
@@ -67,31 +67,33 @@ int main() {
     float max_bc = 0.0;
     // ===================================================== MAIN CODE =================================================== 
     clock_t begin = clock();
-	for(int i = 0; i < nodes; ++i) {
-		// stack<int> s;
+	for(long long int i = 0; i < nodes; ++i) {
+		// stack<long long int> s;
 		// s.clear();
         // ============== INIT ========================
-        clear_stack(s);
-        // vector<vector<int> > p(nodes);
-        for(int j = 0; j < nodes; ++j){
-            if(!p[j].empty()) p[j].clear();
+        // clear_stack(s);
+        stack<long long int> s;
+        // vector<vector<long long int> > p(nodes);
+        for(long long int j = 0; j < nodes; ++j){
+            p[j].clear();
         }
-		// vector<int> sigma(nodes,0);
-        memset(sigma,0,sizeof(sigma));
-		// vector<int> dist(nodes);
-        for(int j=0;j<nodes;++j) dist[j]=-1;
+		// vector<long long int> sigma(nodes,0);
+        // memset(sigma,0,sizeof(sigma));
+		// vector<long long int> dist(nodes);
+        for(long long int j=0;j<nodes;++j) dist[j]=-1,sigma[j]=0,delta[j]=0.0;
 		sigma[i]=1;
 		dist[i]=0;
-		// queue<int> q;
+		// queue<long long int> q;
         // q.clear();
-		clear(q);
+		// clear(q);
+        queue<long long int> q;
         // ============== distance and sigma calculations ====================
         q.push(i);
 		while(!q.empty()){
-			int cons=q.front();
+			long long int cons=q.front();
 			q.pop();
 			s.push(cons);
-			for(int j = V[cons]; j < V[cons+1]; ++j){
+			for(long long int j = V[cons]; j < V[cons+1]; ++j){
 				if(dist[E[j]]<0){
 					dist[E[j]]=dist[cons]+1;		
                     q.push(E[j]);
@@ -104,12 +106,12 @@ int main() {
 		}
         // ============== BC calculation ========================
         // vector<float> delta(nodes,0.0);
-        memset(delta,0,sizeof(delta));
+        // memset(delta,0,sizeof(delta));
         while(!s.empty()){
-            int cons=s.top();
+            long long int cons=s.top();
             s.pop();
-            int upto = p[cons].size();
-            for(int j = 0; j < upto;++j){
+            long long int upto = p[cons].size();
+            for(long long int j = 0; j < upto;++j){
                 delta[p[cons][j]]+=(((float)sigma[p[cons][j]]/sigma[cons])*((float)1+delta[cons]));
             }
             if(cons!=i) {
@@ -122,16 +124,17 @@ int main() {
 
     // ===================================================== RESULTS ===================================================
     // BC values for all nodes
-	// for (int i = 0; i < nodes; ++i) {
+	// for (long long int i = 0; i < nodes; ++i) {
     //     cout<<"Node: "<<i<<"  BC: ";
     //     cout<<fixed<<setprecision(6)<<cb[i]/2.0<<"\n";
     // }
     cout<<"\n";
 
-    // Print the time for execution
+    // Prlong long int the time for execution
     double elapsed_time = double(end - begin) / CLOCKS_PER_SEC;
     cout<<"Execution time: "<<elapsed_time<<endl;
     
+    // ================================ Max BC value ====================================
     // Max BC value
     cout<<"Max BC value: "<<max_bc/2.0<<endl;
 
